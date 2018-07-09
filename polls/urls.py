413d94bff93f3161226706e21c15c6aa11964d55
Joshua Russell <jkrussell756@gmail.com>
2018-07-09 10:33:04 -0400

    made some additional views for the polls app

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # All polls
     path('', views.index, name='index'),
+    # Single poll
+    path('<int:question_id>/', views.detail, name='detail'),
+    # Results for a single poll
+    path('<int:question_id>/results/', views.results, name='results'),
+    # Vote on a poll
+    path('<int:question_id>/vote', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -9,3 +9,10 @@ def index(request):
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the response to question %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s" % question_id)
