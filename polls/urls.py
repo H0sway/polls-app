334f241c97d2c7a2b7b296148fd3918bab4477e9
Joshua Russell <jkrussell756@gmail.com>
2018-07-13 16:22:14 -0400

    Forgot to change the urls when I changed the views

--- polls/urls.py
+++ polls/urls.py
@@ -3,13 +3,14 @@ from django.urls import path
 from . import views
 
 app_name = 'polls'
+app_name = 'polls'
 urlpatterns = [
     # All polls
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     # Single poll
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # Results for a single poll
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # Vote on a poll
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
