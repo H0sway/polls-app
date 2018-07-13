24b803d4c4a115236b44164451d4c0edc9f56555
Joshua Russell <jkrussell756@gmail.com>
2018-07-13 16:08:24 -0400

    added some additional tests for the question index view. makes sure questions pulished in the past are displayed and ones in the future aren't

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 # Import modules
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
 from django.views import generic
 from django.urls import reverse
 
@@ -13,8 +14,12 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions not including those to be published in the future.
+        """
+        return Question.objects.filter(
+        pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
