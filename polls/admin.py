9eb240efef90e6e6237738559577b0c1fb3e1adb
Joshua Russell <jkrussell756@gmail.com>
2018-07-13 16:47:50 -0400

    Changed up the admin page, added in Choice modification and fieldsets for the Question form

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+class ChoiceInLine(admin.StackedInLine):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,                  {'fields': ['question_text']}),
+        ('Date Information',    {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInLine]
+
+admin.site.register(Question, QuestionAdmin)
